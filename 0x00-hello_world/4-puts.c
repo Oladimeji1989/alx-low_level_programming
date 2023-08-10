c5fc72aabaea7e82af2f8d369d5dbffa6d7a600e
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-10 08:52:05 -0700

    Using betty to print a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using puts function to print a line
+ * Retune: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Progamming is like building a multilingual puzzle");
+	return (0);
+}
