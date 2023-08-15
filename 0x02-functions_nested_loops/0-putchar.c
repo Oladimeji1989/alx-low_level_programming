4c62450172018364d26ecf35a15fb40f75b47a96
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-15 12:12:45 -0700

    ptint _putchar using main.h header

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar using putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int A;
+
+	char B[] = "_putchar";
+
+	for (A = 0; A <= 8; A++)
+
+	{
+		_putchar(B[A]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
