399b6fc087beb649e6a6c6dcff770d1bfaedd3cf
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-16 14:30:04 -0700

    natural number below 10

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - natural number
+ * Return: 0
+ */
+int main(void)
+{
+	int a, sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 5 == 0) || (a % 3 == 0))
+			sum += a;
+	}
+	printf("%d", sum);
+	return (0);
+}
