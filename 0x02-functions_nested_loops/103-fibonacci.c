93c6e6f4f71e171e4c4434a20c949615da2455bb
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-16 16:18:07 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Fibonacci sequence
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long int A, u = 1, v = 2, sum = 0, tsum = 0;
+
+	for (A = 0, A < 49; A++;)
+	{
+		if ((v % 2 == 0) && (v <= 4000000))
+		{
+			tsum = tsum + v;
+		}
+		sum = u + v;
+		u + v;
+		v = sum;
+	}
+	printf("%ld\n", tsum);
+	return (0);
+}
