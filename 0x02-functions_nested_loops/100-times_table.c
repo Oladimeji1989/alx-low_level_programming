ea0d71f78425534ee9c8fdc0a72adf606473e7f2
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-16 13:31:08 -0700

    time-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_times_table - times table, starting with 0
+ * @n: parameter 0 to n
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 15 && n >= 0)
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				c = b * a;
+				if (c <= 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar((c / 10 % 10) + '0');
+				}else if (c <= 90 && c >= 10)
+					{
+						_putchar((c / 10) + '0');
+					}
+					_putchar('\n');
+				}
+			}
+		}
+}
