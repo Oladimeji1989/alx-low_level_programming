044b5ec766a910d797837674f2ca4ea662c44c18
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-17 13:46:09 -0700

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -functions that multiplies two integers
+ * @a: parameter a
+ * @b: Parameter b
+ * Return: A
+ */
+int mul(int a, int b)
+{
+	 int A = a * b;
+
+	return (A);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_HEADER_FILE
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
