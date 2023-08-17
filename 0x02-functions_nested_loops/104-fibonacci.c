b7a09f656039ba89a89e9270e00273bea2087ad0
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-16 17:10:08 -0700

    first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - first 98 Fibonacci numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	unsigned long int a, b, c, d, e, f, g;
+
+	b = 1;
+	c = 2;
+
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
+	{
+		printf(", %lu", c);
+		c = c = b;
+		b = c - b;
+	}
+	d = d / 1000000000;
+		e = b % 1000000000;
+		f = c / 1000000000;
+		g = c % 1000000000;
+
+		for (a = 42; a < 99; a++)
+		{
+			printf(", %lu", f + (g / 1000000000));
+			printf("%lu", g % 1000000000);
+			f = f + d;
+			d = f - d;
+			g = g + e;
+			e = g - e;
+		}
+		printf("\n");
+		return (0);
+
+
+}
+
