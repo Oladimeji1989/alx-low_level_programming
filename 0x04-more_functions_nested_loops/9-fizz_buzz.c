390ddb1e0ce0718952eb2e91573ae0ec090614e6
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-18 10:44:20 -0700

    print FizzBuzz mix with digit using printf

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - digits mix with Fizz Buzz
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
