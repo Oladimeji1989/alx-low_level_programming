8dfc7d8c2ce8248ff45002b233618b325b640d77
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-20 13:03:22 -0700

    prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,23 +15,22 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				c = b * a;
+				c = a * b;
 				if (c <= 99)
 					_putchar(' ');
-					if (c <= 9)
+				if (c <= 9)
 					_putchar(' ');
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
 					_putchar((c / 10 % 10) + '0');
 				}
-					else if (c <= 99 && c >= 10)
-					{
-						_putchar((c / 10) + '0');
-					}
-					_putchar((c % 10) + '0');
+				else if (c <= 99 && c >= 10)
+				{
+					_putchar((c / 10) + '0');
 				}
-			_putchar("\n" ,);
+				_putchar((c % 10) + '0');
 			}
+			_putchar('\n');
 		}
 }
