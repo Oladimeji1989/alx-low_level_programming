523d36c2a668b6e038e354ed20354496ffc908b2
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-08-23 13:43:50 -0700

    encoding a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @str: str parameter
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int a, b;
+	char c[] = "aAeEoOtTlL";
+	char d[] = "4433007711";
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		for (b = 0; c[b] != '\0'; b++)
+		{
+			if (str[a] == c[b])
+			{
+				str[a] = d[b];
+			}
+		}
+	}
+	return (str);
+}
