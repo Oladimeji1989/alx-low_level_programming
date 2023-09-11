98168a806e0068b53e4e40087a0b5d70c24ed666
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-09-11 07:26:34 -0700

    sum two integers through header file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef MACROLIKE_HEADER_FILE
 #define MACROLIKE_HEADER_FILE
 /**
- * File - 3-function_like_macro
+ * File - 3-function_like_macro.h
  * @x: value parameter
  */
 #define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef MACROSUM_HEADER_FILE
+#define MACROSUM_HEADER_FILE
+/**
+ * File - 4-sum.h
+ * @x: x number parameter
+ * @y: y number parameter
+ */
+#define SUM(x, y) ((x) + (y))
+#endif
