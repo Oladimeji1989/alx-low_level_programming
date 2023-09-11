3cbf09b2bf462ff6a781aa0a331158d60515a0a0
*Oladimeji1989* <*adewumidavid21@gmail.com*>
2023-09-11 08:21:50 -0700

    creating struct for dog information

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# ALX project on c programming
+* 0x0E. C - Structures, typedef
+
+## General Objective Learning
+* What are structures, when, why and how to use them
+* How to use typedef
+
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+/**
+ * dog - creating dog infomation using struct
+ * @name: dog name parameter
+ * @age: dog age parameter
+ * @owner: dog owner parameter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
